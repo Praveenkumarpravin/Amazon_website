3f027128496b1bee8175ae4021e0b30a31aaf3a2
Praveenkumarpravin <ppalraj361@gmail.com>
2025-07-06 10:22:34 +0530

    changed packaaje.json and vite config and package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/AmazonProject/', // <-- change this to match your GitHub repo name
   plugins: [react()],
-  server: {
-    port: "3000"
-  }
 })
